c123356cd4b657a57734b6abed7cfa2e63278642
skywings-kor <59461034+skywings-kor@users.noreply.github.com>
2020-07-07 23:54:17 +0900

    날짜js파일, 로그인 sql 기능
    날짜 관련 자바스크립트 파일, SQL 이용시 로그인을 위한 sql 기능 선탑제

---
+++ Src/html/date.js
@@ -0,0 +1,17 @@
+
+function systemdate()
+{
+    let today = new Date();
+
+    let year = today.getFullYear();
+
+    let month = today.getMonth() + 1;
+
+    let date = today.getDate();
+
+    let day = today.getDay();
+
+    console.log('kkk');
+
+}
+
